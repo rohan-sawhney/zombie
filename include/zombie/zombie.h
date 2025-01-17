e3a4dfd68c5e5fdf9e2a471d1a64a768ddfd8245
Rohan Sawhney <sawhney_rohan@yahoo.co.in>
2025-01-16 17:42:38 -0800

    rename sampler files

--- include/zombie/zombie.h
+++ include/zombie/zombie.h
@@ -2,8 +2,8 @@
 
 #include <zombie/point_estimation/walk_on_spheres.h>
 #include <zombie/point_estimation/walk_on_stars.h>
-#include <zombie/variance_reduction/boundary_sampler.h>
-#include <zombie/variance_reduction/domain_sampler.h>
+#include <zombie/variance_reduction/boundary_samplers.h>
+#include <zombie/variance_reduction/domain_samplers.h>
 #include <zombie/variance_reduction/boundary_value_caching.h>
 #include <zombie/variance_reduction/reverse_walk_solver.h>
 #include <zombie/utils/fcpw_geometric_queries.h>
